41debc727f84fbd256d36e0b0aa195d2502f2abd
EDToaster <edt.chen@mail.utoronto.ca>
2018-07-20 17:10:39 -0700

    Initial Commit

---
+++ example/ToasterLinear_Dart_example.dart
@@ -0,0 +1,6 @@
+import 'package:ToasterLinear_Dart/ToasterLinear_Dart.dart';
+
+main() {
+  var awesome = new Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/ToasterLinear_Dart.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library ToasterLinear_Dart;
+
+export 'src/ToasterLinear_Dart_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
